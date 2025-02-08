c5b3a47d0c2fbfe5e26e44e9e7d356731e112266
Phil Stubbs <philipstubbs13@gmail.com>
2025-02-08 11:30:34 -0600

    sorting products

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -104,7 +104,14 @@ export async function getAllProducts({
       ...priceFilter,
       ...ratingFilter,
     },
-    orderBy: { createdAt: "desc" },
+    orderBy:
+      sort === "lowest"
+        ? { price: "asc" }
+        : sort === "highest"
+        ? { price: "desc" }
+        : sort === "rating"
+        ? { rating: "desc" }
+        : { createdAt: "desc" },
     skip: (page - 1) * limit,
     take: limit,
   });
